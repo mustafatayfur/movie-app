8f424aa752b7fedcb35239e92672a627b84f7fde
mustafa <mltm0204@gmail.com>
2021-12-31 21:17:41 +0300

    the project is completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280'
 const defaultImage = 'https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80'
 
 const MovieCard = ({title, poster_path, overview, vote_average }) => {
+
+    const {currentUser} = useContext(AuthContext);
 
     const setVoteClass = (vote) => {
         if(vote >= 8){
@@ -21,7 +25,12 @@ const MovieCard = ({title, poster_path, overview, vote_average }) => {
             
             <div className='movie-info'>
                 <h3>{title}</h3>
-                <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+
+                {
+                    currentUser ? <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span> : null
+
+                }
+
             </div>
             <div className="movie-over">
                 <h2>Overview</h2>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import MovieCard from '../components/MovieCard'
+import { AuthContext } from '../context/AuthContext'
 
 const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=26fa018efa9f5f9c0a7d64a48d1d0e06'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=26fa018efa9f5f9c0a7d64a48d1d0e06&query='
@@ -8,6 +9,7 @@ const Main = () => {
 
 const [movies, setMovies] = useState([])
 const [searchTerm, setSearchTerm] = useState('')
+const currentUser = useContext(AuthContext)
 
 useEffect(()=>{
     getMovies(FEATURED_API)
@@ -21,8 +23,10 @@ const getMovies = (API)=> {
 
 const handleSubmit = (e)=> {
     e.preventDefault()
-    if(searchTerm){
+    if(searchTerm && currentUser){
         getMovies(SEARCH_API + searchTerm)
+    }else{
+        alert('Please login to search movie')
     }
     setSearchTerm('')
 }
